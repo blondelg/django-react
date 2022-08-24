b582ca338e64fb3eebd58260060782c127fee2fe
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2022-08-24 16:54:42 +0200

    draft front

--- frontend/src/services/ApiService.tsx
+++ frontend/src/services/ApiService.tsx
@@ -1,41 +1,77 @@
-import { IAuthTokens, TokenRefreshRequest, applyAuthTokenInterceptor } from 'axios-jwt'
-import axios from 'axios'
+import {
+  IAuthTokens,
+  TokenRefreshRequest,
+  applyAuthTokenInterceptor,
+} from "axios-jwt"
+import axios from "axios"
+import {
+  isLoggedIn,
+  setAuthTokens,
+  clearAuthTokens,
+  getAccessToken,
+  getRefreshToken,
+} from "axios-jwt"
 
-const BASE_URL = 'https://api.example.com'
+const BASE_URL = "http://localhost:8000/api"
 
 export const axiosInstance = axios.create({ baseURL: BASE_URL })
 
-const requestRefresh: TokenRefreshRequest = async (refreshToken: string): Promise<IAuthTokens | string> => {
-  const response = await axios.post(`${BASE_URL}/auth/refresh_token`, { token: refreshToken })
+const requestRefresh: TokenRefreshRequest = async (
+  refreshToken: string
+): Promise<IAuthTokens | string> => {
+  const response = await axios.post(
+    `${BASE_URL}/token/refresh/`,
+    { token: refreshToken },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
 
-  // If your backend supports rotating refresh tokens, you may also choose to return an object containing both tokens:
-  // return {
-  //  accessToken: response.data.access_token,
-  //  refreshToken: response.data.refresh_token
-  //}
-
-  return response.data.access_token
+  return {
+    accessToken: response.data.access,
+    refreshToken: response.data.refresh,
+  }
 }
 
-// 3. Add interceptor to your axios instance
+interface LoginRequest {
+  username: string
+  password: string
+}
+
+export const login = async (params: LoginRequest) => {
+  const response = await axiosInstance.post("/token/", params)
+
+  console.log("LOGIN RESPONSE", response)
+
+  setAuthTokens({
+    accessToken: response.data.access_token,
+    refreshToken: response.data.refresh_token,
+  })
+}
+
+const logout = () => clearAuthTokens()
+
 applyAuthTokenInterceptor(axiosInstance, { requestRefresh })
 
+export class Params {[key: string]: string}
+
 export class ApiService {
+  get(url: string, params = new Params()) {
+    return axiosInstance.get(url, {params: params})
+  }
 
-    get() {
+  post(url: string, object: object) {
+    return axiosInstance.post(url, object)
+  }
 
-    }
+  put(url: string, object: object) {
+    return axiosInstance.put(url, object)
+  }
 
-    post() {
+  delete(url: string, object: object) {
+    return axiosInstance.delete(url)
+  }
 
-    }
-
-    put() {
-
-    }
-
-    delete() {
-
-    }
-    
 }
